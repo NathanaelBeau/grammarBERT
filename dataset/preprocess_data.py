7a72f8c41df9566f3cdc56a163cdcba24f521634
wamoe <ufk18hl@jean-zay1.idris.fr>
2023-11-21 16:30:02 +0100

    add filter lenght sequence and multi thread to preprocessing + environment file

--- dataset/preprocess_data.py
+++ dataset/preprocess_data.py
@@ -1,47 +1,80 @@
 import json
 import ast
-
 import numpy as np
-
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from datasets import load_dataset
 from asdl.ast_operation import ast2seq
 from asdl.grammar import Grammar, GrammarRule, ReduceAction
 
+# Constante pour la taille des lots
+BATCH_SIZE = 100
+# Nombre de threads à utiliser pour le ThreadPoolExecutor
+NUM_THREADS = 4
 
 def get_data():
     ds = load_dataset("bigcode/the-stack", data_dir="data/python", split="train", streaming=True)
-    return ds
+    # Ajouter un filtre pour ne garder que les exemples avec moins de 200 tokens (à adapter selon le besoin)
+    filtered_ds = (sample for sample in ds if len(sample['content'].split()) < 200)
+    return filtered_ds
 
-def preprocess_examples(dataset):
-    act_dict, primitives = get_act_dict()
+def preprocess_example(sample, act_dict, primitives):
+    try:
+        code = sample['content']
+        example = {'code': code}
+
+        python_ast = ast.parse(code)
+        action_seq_grammar, _, _, _ = ast2seq(python_ast, act_dict, primitives=primitives)
+        actions_seq = [
+            action[0].label if isinstance(action[0], GrammarRule) or isinstance(action[0], ReduceAction)
+            else (str(action[0]))
+            for action in action_seq_grammar]
+
+        example['action_seq'] = actions_seq
+        return example
+    except Exception as e:
+        return None
+
+def preprocess_batch(batch, act_dict, primitives):
+    processed_batch = []
+    for sample in batch:
+        processed_example = preprocess_example(sample, act_dict, primitives)
+        if processed_example:
+            processed_batch.append(processed_example)
+    return processed_batch
+
+def preprocess_examples(dataset, act_dict, primitives):
     len_code = []
     pass_examples = 0
+    output_data = []
+
+    # Prétraiter les données par lots en utilisant ThreadPoolExecutor
+    with ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+        # Création d'une liste pour les futurs résultats
+        futures = []
+        batch = []
+
+        for sample in dataset:
+            batch.append(sample)
+            if len(batch) == BATCH_SIZE:
+                futures.append(executor.submit(preprocess_batch, batch, act_dict, primitives))
+                batch = []
+
+        # Ajouter le dernier lot si nécessaire
+        if batch:
+            futures.append(executor.submit(preprocess_batch, batch, act_dict, primitives))
+
+        # Récupérer les résultats à mesure qu'ils sont terminés
+        for future in as_completed(futures):
+            processed_batch = future.result()
+            output_data.extend(processed_batch)
+            len_code.extend([len(example['action_seq']) for example in processed_batch if example])
+
+    # Écriture des données traitées en bloc dans le fichier
     with open('dataset/output_data.jsonl', 'w') as output_file:
-        for index, sample in enumerate(iter(dataset)):
-            try:
-                code = sample['content']
-                example = {'code': code}
-
-                python_ast = ast.parse(code)
-                action_seq_grammar, _, _, _ = ast2seq(python_ast, act_dict, primitives=primitives)
-                actions_seq = [
-                    action[0].label if isinstance(action[0], GrammarRule) or isinstance(action[0], ReduceAction)
-                    else (str(action[0]))
-                    for action in action_seq_grammar]
-
-                len_code.append(len(actions_seq))
-
-                example['action_seq'] = actions_seq
-                json.dump(example, output_file)  # write processed example to file
-                output_file.write('\n')  # add newline
-
-            except:
-                pass_examples += 1
-                continue
-
-        print('number of example', index - pass_examples)
-        print('mean', np.mean(len_code))
-        print('var', np.var(len_code))
+        for example in output_data:
+            if example:  # Assurez-vous que l'exemple n'est pas None
+                json.dump(example, output_file)
+                output_file.write('\n')
 
 def get_act_dict(path='./asdl/PythonASDLgrammar3,9.txt'):
     asdl_text = open(path).read()
@@ -51,10 +84,3 @@ def get_act_dict(path='./asdl/PythonASDLgrammar3,9.txt'):
     Reduce = ReduceAction('Reduce')
     act_dict = dict([(act.label, act) for act in act_list])
     return act_dict, terminal_types
-
-if __name__ == '__main__':
-
-    dataset = get_data()
-
-    preprocess_examples(dataset)
-
--- get_data.py
+++ get_data.py
@@ -1,5 +1,10 @@
-from dataset.preprocess_data import get_data, preprocess_examples
+from dataset.preprocess_data import get_data, preprocess_examples, get_act_dict
 
+# Récupère le dataset filtré
 dataset = get_data()
 
-preprocess_examples(dataset)
+# Récupère les dictionnaires d'actions et les primitives
+act_dict, primitives = get_act_dict()
+
+# Prétraitement des exemples avec les arguments nécessaires
+preprocess_examples(dataset, act_dict, primitives)
