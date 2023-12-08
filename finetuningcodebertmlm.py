c9e1794083a5c0731285328d13bae6759eb2e496
w.ameur <w.ameur@groupeonepoint.com>
2023-12-08 16:36:02 +0100

    fixing eval loss nan on finetuningcodebertmlm

--- finetuningcodebertmlm.py
+++ finetuningcodebertmlm.py
@@ -1,4 +1,4 @@
-from transformers import RobertaConfig, RobertaTokenizer, RobertaForMaskedLM, pipeline, AutoTokenizer,  DataCollatorForLanguageModeling, TrainingArguments, Trainer, TrainerCallback
+from transformers import RobertaForMaskedLM, RobertaTokenizer, DataCollatorForLanguageModeling, TrainingArguments, Trainer, TrainerCallback
 import json
 from torch.utils.data import Dataset, DataLoader
 import random
@@ -8,16 +8,21 @@ from asdl.ast_operation import Grammar, GrammarRule, ReduceAction
 
 
 
+# Model and tokenizer initialization
 model_checkpoint = "microsoft/codebert-base-mlm"
 model = RobertaForMaskedLM.from_pretrained(model_checkpoint)
 tokenizer = RobertaTokenizer.from_pretrained(model_checkpoint)
-
 
 
 # loading data from jsonl file
 jsonl_file = 'dataset/output_data.jsonl'
 with open(jsonl_file, 'r') as file:
     full_data = [json.loads(line) for line in file]
+
+
+#Checking loaded data
+print(f"Number of data examples loaded : {len(full_data)}")
+
 
 # retrieve all grammar actions in a list
 asdl_text = open('./asdl/PythonASDLgrammar3,9.txt').read()
@@ -27,16 +32,17 @@ assert (len(grammar) == len(act_list))
 Reduce = ReduceAction('Reduce')
 act_dict = dict([(act.label, act) for act in act_list])
 
-# increase the vocabulary of Bert model and tokenizer
+# # increase the vocabulary of Bert model and tokenizer
 new_tokens = act_dict
 num_added_toks = tokenizer.add_tokens(new_tokens)
 
 model.resize_token_embeddings(len(tokenizer))
 
+
 # Dataset class from pytorch
 class CodeDataset(Dataset):
-    def __init__(self, jsonl_file, model_checkpoint, max_length=512):
-        self.data = full_data
+    def __init__(self, data, tokenizer, max_length=512):
+        self.data = data
         self.tokenizer = tokenizer
         self.max_length = max_length
 
@@ -46,61 +52,52 @@ class CodeDataset(Dataset):
 
     def __getitem__(self, idx):
         code, action_seq = self.data[idx]
-
-
         encoded_code = self.tokenizer.encode_plus(
             code,
-            add_special_tokens=True,
-            max_length=self.max_length,
-            padding='max_length',
-            truncation=True,
-            return_tensors='pt'
-        )
-
-
+            add_special_tokens=True, 
+            max_length=self.max_length, 
+            padding='max_length', 
+            truncation=True, 
+            return_tensors='pt')
+        
         encoded_actions = self.tokenizer.encode_plus(
-            action_seq,
-            add_special_tokens=True,
-            max_length=self.max_length,
-            padding='max_length',
-            truncation=True,
-            return_tensors='pt'
-        )
+            action_seq, 
+            add_special_tokens=True, 
+            max_length=self.max_length, 
+            padding='max_length', 
+            truncation=True, 
+            return_tensors='pt')
+        
 
-        return {
-            'input_ids': encoded_code['input_ids'].squeeze(),
-            'attention_mask': encoded_code['attention_mask'].squeeze(),
-            'labels': encoded_actions['input_ids'].squeeze()
-        }
-
+        return {'input_ids': encoded_code['input_ids'].squeeze(), 
+                'attention_mask': encoded_code['attention_mask'].squeeze(), 
+                'labels': encoded_actions['input_ids'].squeeze()
+            }
 
 
 
 
 # train test split
-
 random.shuffle(full_data)
-
-
 train_size = int(0.8 * len(full_data))
-test_size = len(full_data) - train_size
+train_data, test_data = full_data[:train_size], full_data[train_size:]
 
 
-train_data = full_data[:train_size]
-test_data = full_data[train_size:]
-
+#print training and testing dataset
 print(f"Training dataset size : {len(train_data)}")
-print(f"Training dataset test: {len(test_data)}")
+print(f"Testing dataset size: {len(test_data)}")
 
 
 
+# Creating DataLoaders
 train_dataset = CodeDataset(train_data, tokenizer)
 test_dataset = CodeDataset(test_data, tokenizer)
 
-train_loader = DataLoader(CodeDataset(train_data, tokenizer), batch_size=4, shuffle=True)
-test_loader = DataLoader(CodeDataset(test_data, tokenizer), batch_size=4, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=2, shuffle=False)
 
 
+# Creating Datacollator
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
 
 
@@ -154,71 +151,45 @@ data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probabi
 
 
 
-##############train test split with hf method###############
 
+###########Training arguments and trainer config################################
 
-
-# downsampled_dataset = dataset["train"].train_test_split(
-#     train_size=train_size, test_size=test_size, seed=42
-# )
-
-
-
-# batch_size = 16
-# # Show the training loss with every epoch
-# logging_steps = len(dataset["train"]) // batch_size
-# model_name = model_checkpoint.split("/")[-1]
-
-# train_loader = DataLoader(CodeDataset(train_data, tokenizer), batch_size=16, shuffle=True)
-# test_loader = DataLoader(CodeDataset(test_data, tokenizer), batch_size=16, shuffle=True)
-
-
-###########Training arguments and trainer configs################################
-
-
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!a tester!!!!!!!!!!!!!!!!!!!!!!!!!!
-# def generate_and_save_model_outputs(model, dataloader, tokenizer, file_path):
-#     model.eval()  # Mettre le modèle en mode évaluation
-#     with open(file_path, "w") as file:
-#         with torch.no_grad():
-#             for batch in dataloader:
-#                 inputs = {k: v.to(model.device) for k, v in batch.items() if k != 'labels'}
-#                 outputs = model.generate(**inputs)
-#                 for output in outputs:
-#                     decoded_output = tokenizer.decode(output, skip_special_tokens=True)
-#                     file.write(decoded_output + "\n")
-
-
-# class GenerateOutputsCallback(TrainerCallback):
-#     def on_evaluate(self, args, state, control, **kwargs):
-#         generate_and_save_model_outputs(kwargs['model'], test_loader, tokenizer, "model_generated_outputs.txt")
 
 training_args = TrainingArguments(
     output_dir=f"{model_checkpoint}-finetuned-codebertmlm",
-    overwrite_output_dir=True,
-    evaluation_strategy="epoch",
-    learning_rate=2e-5,
-    weight_decay=0.01,
-    per_device_train_batch_size=4,
-    per_device_eval_batch_size=4,
-    push_to_hub=False,
+    evaluation_strategy="epoch", 
+    learning_rate=2e-5, 
+    weight_decay=0.01, 
+    per_device_train_batch_size=2, 
+    per_device_eval_batch_size=2, 
+    num_train_epochs=3,
+    push_to_hub=False,  
     fp16=False,
-    logging_steps=len(train_loader),
+    logging_steps=len(train_loader)
+    
 )
 
+
+
+# Callback for debugging
+class DebugCallback(TrainerCallback):
+    def on_log(self, args, state, control, logs=None, **kwargs):
+        print(logs)
 
 
 
 trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_loader.dataset,
-    eval_dataset=test_loader.dataset,
-    data_collator=data_collator,
-    tokenizer=tokenizer
-    # callbacks=[GenerateOutputsCallback()]
+    model=model, 
+    args=training_args, 
+    train_dataset=train_loader.dataset, 
+    eval_dataset=test_loader.dataset, 
+    data_collator=data_collator, 
+    tokenizer=tokenizer, 
+    callbacks=[DebugCallback()]
 
 )
+
+
 
 
 trainer.train()
