3760fb78002c908401f0c748487d0740dae02b21
Jonor127-OP <n.beau@groupeonepoint.com>
2024-01-18 12:03:47 +0100

    add slurm script to getdata to fast training

--- getdata.sh
+++ getdata.sh
@@ -2,19 +2,30 @@
 
 #SBATCH --job-name=getdata
 
+#SBATCH --qos=qos_gpu-t4
+
 #SBATCH --output=./logfiles/getdata.out
 
 #SBATCH --error=./logfiles/getdata.err
 
+#SBATCH --time=50:30:00
+
 #SBATCH --ntasks=4
 
-#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
 
-#SBATCH --partition=qos_gpu-t4
+#SBATCH --cpus-per-task=10
 
-#SBATCH --time=60:00:00
+#SBATCH --hint=nomultithread
 
-module load anaconda-py3/2020.11
+#SBATCH --constraint=v100-32g
+
+module purge
+module load anaconda-py3/2019.03
 conda activate grammarBERT
 
+set -x
+nvidia-smi
+
 srun python get_data.py
+
